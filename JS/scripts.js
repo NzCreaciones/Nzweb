254d3a02dd7881e8a4543adc7324306e3ea0c2cc
nerviozzo <raupulus@gmail.com>
2015-10-26 01:18:01 +0100

    Ajustes en estilos, diseño y relaciones de enlaces

--- JS/scripts.js
+++ JS/scripts.js
@@ -13,3 +13,32 @@ function fondocirculo(nombreimagen)
 
 
 */
+
+//Logo en el título, cambiarlo por su reverso
+function cambiarlogo() {
+	document.getElementById("logoimg").src = "../images/logo_dos.png"
+}
+
+function cambiarlogoOFF() {
+	document.getElementById("logoimg").src = "../images/logo.png"
+}
+
+//Desplegar menú de la Derecha
+function menuderecha() {
+	var cajaderecha = document.getElementById("cajaderecha").style
+
+	if (cajaderecha.right != "0px") {
+		cajaderecha.right = "0px";
+
+	/*
+		cajaderecha.transition
+	-moz-transition-duration: 1s;
+	-webkit-transition-duration: 1s;
+	-o-transition-duration: 1s;
+	-ms-transition-duration: 1s;
+	*/
+
+	} else if (cajaderecha.right == "0px") {
+		cajaderecha.right = "-280px";
+	}
+}
